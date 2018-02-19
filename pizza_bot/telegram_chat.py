a51e7ba2b2d6dbdcac231b054b45aed367d4b472
Blank Wuoo <blank2799@gmail.com>
2018-02-19 15:46:40 +0200

    Fixed bot intergation with telegram

--- pizza_bot/telegram_chat.py
+++ pizza_bot/telegram_chat.py
@@ -29,12 +29,14 @@ class TelegramDialog(Dialog):
         """Send message to user"""
         if self.chat is None:
             raise TypeError('No chat to send a message')
-        self.bot.send_message(message, chat=self.chat)
+        self.bot.send_message(self.chat, message)
 
     def process_update(self, update: telegram.Update):
+        if update.message is None:
+            raise TypeError('Update with empty message')
         if self.chat is None:
-            self.chat = update.effective_chat
-        elif self.chat != update.effective_chat:
+            self.chat = update.message.chat_id
+        elif self.chat != update.message.chat_id:
             raise TypeError('Chat integrity error')
 
         if update.message is None:
